4bfda08d071719cc562a0a91f21563952deb1cb2
_kentho <kenneththomsennmbk@gmail.com>
2026-05-18 20:30:45 -0700

    A script that states types and casts types.

---
+++ type_casting.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#we define our var
+number1 = 3
+number2 = 3.0
+number3 = 1
+#we print their type
+print(type(number1))
+print(type(number2))
+print(type(number3))
+#we cast a different type
+print(float(number1))
+print(int(number2))
+print(bool(number3))
+
