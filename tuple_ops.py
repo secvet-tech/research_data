70021335044478b441622c0fb0b08bcfd59f9891
_kentho <kenneththomsennmbk@gmail.com>
2026-05-24 14:55:28 -0700

    added an operation for concatenation, slicing to follow

---
+++ tuple_ops.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import datetime
+names_members = ("Elio", "Joss", "kentho")
+newest_members = ("Al", "Ed")
+#then we make 2 into 1
+all_members = names_members + newest_members
+#we print
+print(all_members)
+#next we slice and add datetime
+og_members = names_members + (datetime.datetime.now().isoformat(),)
+#print
+print(og_members)
