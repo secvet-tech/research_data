c1aed601f89bc97b927f7f4a6bdbfc6edcd12671
_kentho <kenneththomsennmbk@gmail.com>
2026-05-24 19:05:04 -0700

    added a sets file with a union op

---
+++ sets.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#we create our dictionary
+classic_rock = {"AC/DC", "Black Sabbath", "Fleetwood Mac"}
+rap = {"Run The Jewel", "T.I.", "Aesop Rock"}
+#we bind the 2 with union
+some_artists = classic_rock.union(rap)
+#we print
+print(some_artists)
