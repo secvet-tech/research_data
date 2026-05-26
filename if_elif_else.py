bf40dcd01b73414c1857164b348b1874c4976af9
_kentho <kenneththomsennmbk@gmail.com>
2026-05-25 17:35:20 -0700

    added a few conditions to an app that accepts userage arguments.

---
+++ if_elif_else.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+input_age = input("enter your age...")
+input_age_copy = int(input_age)
+#we create conditions
+if input_age_copy > 18:
+    print("smoke up!")
+elif input_age_copy == 18:
+    print("happy belated!")
+else:
+    print("go play")
+print("smoking is a bad habit...")
